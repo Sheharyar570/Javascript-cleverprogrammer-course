0011e30c2dff6d392c75fd8cee03d3da8a2dc2fd
Sheharyar Ahmad <sheharyar.572@gmail.com>
2020-11-08 22:04:21 +0500

    Challenge 2 complete

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,10 @@ function ageInDays() {
 function reset() {
     document.getElementById('ageInDays').remove();
 }
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small"; //https://cdn2.thecatapi.com/images/4g6.gif";
+    div.appendChild(image);
+}
